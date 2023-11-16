b21ca185199e5133b54d1fc1499aff981aaf76e4
ivanLapchenko <ivan.l@serverbee.net>
2023-11-16 22:34:59 +0200

    init

---
+++ main/main/asgi.py
@@ -0,0 +1,25 @@
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter
+from channels.routing import URLRouter
+
+from django.core.asgi import get_asgi_application
+from django.urls import path
+
+
+import os
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'main.settings')
+
+
+from main.consumers import YourConsumer
+
+
+django_asgi_app = get_asgi_application()
+
+application = ProtocolTypeRouter({
+    'http': django_asgi_app,
+    'websocket': AuthMiddlewareStack(
+        URLRouter([
+            path('ws', YourConsumer.as_asgi())
+        ])
+    )
+})
